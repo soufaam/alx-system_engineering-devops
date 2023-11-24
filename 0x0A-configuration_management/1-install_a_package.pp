da5122f55d8dd0553d19d089f2e6f1b7a2a62558
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-24 23:32:09 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,9 +2,16 @@
 package {'python3-pip':
 ensure  => 'installed',
 }
+exec { 'install_werkzeug':
+command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+path    => '/usr/local/bin:/usr/bin',
+creates => '/usr/local/lib/python3.x/dist-packages/Werkzeug-2.1.1.dist-info',
+require => Package['python3-pip'],
+}
+
 exec{ 'install_flask':
 command =>  '/usr/bin/pip3 install flask==2.1.0',
 path    => '/usr/local/bin:/usr/bin',
 creates => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
-require => Package['python3-pip'],
+require => Package['install_werkzeug'],
 }
