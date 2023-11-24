08b33bb1adb4053b4e4099bdbe93e67db76d8f40
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-24 23:45:46 +0100

    1-install_a_package.pp bug fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,12 +6,12 @@ exec { 'install_werkzeug':
 command => '/usr/bin/pip3 install Werkzeug==2.1.1',
 path    => '/usr/local/bin:/usr/bin',
 creates => '/usr/local/lib/python3.8/dist-packages/Werkzeug-2.1.1.dist-info',
-require => Package['python3-pip'],
+require => package['python3-pip'],
 }
 
 exec{ 'install_flask':
 command =>  '/usr/bin/pip3 install flask==2.1.0',
 path    => '/usr/local/bin:/usr/bin',
 creates => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
-require => Package['install_werkzeug'],
+require => package['install_werkzeug'],
 }
