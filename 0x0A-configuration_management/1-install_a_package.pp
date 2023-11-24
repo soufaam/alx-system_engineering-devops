81e13ef1987d15ad9acce80219fd34c444b7ec2c
Soufiane Aamari <Soufianeaamari@gmail.com>
2023-11-24 23:05:34 +0100

    1-install_a_package.pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,8 @@
+# File: 0create_a_file.pp: create a file in /tmp
+file {'/tmp/school':
+ensure  => 'file',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
+}
